a22c2e1e2461c35b4f8a5326d9b04ba977ae7aa2
mzus <morzusman@gmail.com>
2016-01-21 04:25:23 +0200

    PLS

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt-get install -y build-essential cmake
 apt-get install -y make gcc
 apt-get install -y python-dev
 apt-get install -y ctags
+apt-get install -y zsh
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/powerline/fonts.git 
 $HOME/fonts/install.sh
@@ -12,8 +13,8 @@ cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/
 cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
 sudo vim +BundleInstall +qall
 echo Please Press Enter
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo chsh -s %(which zsh)
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp -f $HOME/dot-files/.zshrc $HOME/
+sudo chsh -s $(which zsh)
 $HOME/.vim/YouCompleteMe/install.py --clang-completer 
 echo Please Relogin to apply the changes.
