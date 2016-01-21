6831d6c1192454768c45e1288c95b44d91d2e86b
mzus <morzusman@gmail.com>
2016-01-21 04:07:46 +0200

    sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ git clone https://github.com/greatwing/dot-files.git
 cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/
 cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
 sudo vim +BundleInstall +qall
+echo Please Press Enter
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s %(which zsh)
 cp -f $HOME/dot-files/.zshrc $HOME/
