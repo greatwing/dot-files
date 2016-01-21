25bc003214c8d126f103a048d9212cc5f4c59035
mzus <morzusman@gmail.com>
2016-01-21 05:01:01 +0200

    PLS2

--- install.sh
+++ install.sh
@@ -9,12 +9,13 @@ git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.v
 git clone https://github.com/powerline/fonts.git 
 $HOME/fonts/install.sh
 git clone https://github.com/greatwing/dot-files.git
-cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/
-cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
+cp -f $HOME/dot-files/.vimrc  $HOME/
 sudo vim +BundleInstall +qall
+cp $HOME/dot-files/.ycm_extra_conf.py  $HOME/
+cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
 echo Please Press Enter
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp -f $HOME/dot-files/.zshrc $HOME/
 sudo chsh -s $(which zsh)
-$HOME/.vim/YouCompleteMe/install.py --clang-completer 
+$HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer 
 echo Please Relogin to apply the changes.
