6943f3af10987f26aac18f0ac9ec824790ccc4d6
mzus <morzusman@gmail.com>
2016-01-23 16:02:57 +0200

    changes

--- install.sh
+++ install.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-apt-get install -y curl vim zsh
-apt-get install -y build-essential cmake
-apt-get install -y make gcc
-apt-get install -y python-dev
-apt-get install -y ctags
-apt-get install -y zsh
+apt-get install git curl vim zsh
+apt-get install tmux
+apt-get install build-essential cmake
+apt-get install make gcc
+apt-get install python-dev
+apt-get install ctags
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 git clone https://github.com/powerline/fonts.git 
 $HOME/fonts/install.sh
 git clone https://github.com/greatwing/dot-files.git
-cp -f $HOME/dot-files/.vimrc  $HOME/
-sudo vim +BundleInstall +qall
-cp $HOME/dot-files/.ycm_extra_conf.py  $HOME/
+cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/
 cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
-echo Please Press Enter
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp -f $HOME/dot-files/.zshrc $HOME/
+sudo vim +BundleInstall +qall
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh)
+cp -f $HOME/dot-files/.zshrc $HOME/
 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer 
 echo Please Relogin to apply the changes.
