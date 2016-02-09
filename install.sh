e3b759a2e547ecb0768340f7a2043b2bf8d57365
mzusman <morzusman@gmail.com>
2016-02-09 17:33:46 +0200

    Pls1

--- install.sh
+++ install.sh
@@ -1,27 +1,23 @@
 #/bin/sh
-echo "This script is gonna change your pc configuration completely, continue (y/n) ?"
-read QQ
-if ($QQ == "y") then
-	sudo add-apt-repository ppa:neovim-ppa/unstable
-	sudo apt-get update
-	sudo apt-get install -y git curl zsh vim tmux build-essential cmake make gcc python-dev ctags git-extras \
-		neovim python-pip python3-dev python3-pip
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	git clone https://github.com/powerline/fonts.git $HOME/fonts
-	$HOME/fonts/install.sh
-	cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/dot-files/.ideavimrc $HOME/
-	cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install -y git curl zsh vim tmux build-essential cmake make gcc python-dev ctags git-extras \
+	neovim python-pip python3-dev python3-pip
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/powerline/fonts.git $HOME/fonts
+$HOME/fonts/install.sh
+cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/dot-files/.ideavimrc $HOME/
+cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
 
-	vim +PlugInstall +qall
-	mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-	ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-	ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+vim +PlugInstall +qall
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
-	$HOME/.vim/plugged/YouCompleteMe/install.py --clang-completer 
-	sudo chsh -s $(which zsh)
-	echo "Please Re login to apply the changes."
-	echo "Press Enter."
-	read NOP 
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+$HOME/.vim/plugged/YouCompleteMe/install.py --clang-completer 
+sudo chsh -s $(which zsh)
+echo "Please Re login to apply the changes."
+echo "Press Enter."
+read NOP 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
