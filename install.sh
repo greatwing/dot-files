a9b4931cafd031d7a3bed1c3d7f34da4e1d317b5
Mor Zusman <morzusman@gmail.com>
2016-03-15 17:09:13 +0200

    install fix

--- install.sh
+++ install.sh
@@ -1,24 +1,33 @@
 #/bin/sh
+# install requirements: 
 sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:gnome-terminator
+
 sudo apt-get update
 sudo apt-get install -y git curl zsh vim tmux build-essential cmake make gcc python-dev ctags git-extras \
-	neovim python-pip python3-dev python3-pip
-pip install neovim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/powerline/fonts.git $HOME/fonts
-$HOME/fonts/install.sh
-cp -f $HOME/dot-files/.vimrc  $HOME/dot-files/.ycm_extra_conf.py $HOME/dot-files/.ideavimrc $HOME/
-cp -r $HOME/dot-files/UltiSnips $HOME/.vim/
-vim +PlugInstall +qall
+	neovim python-pip python3-dev python3-pip terminator
+
+sudo pip install neovim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# make links
+ln -sf $HOME/dot-files/.vimrc $HOME/.vimrc
+
+ln -sf  $HOME/dot-files/.ycm_extra_conf.py $HOME/.ycm_extra_conf.py
+ln -sf  $HOME/dot-files/.ideavimrc $HOME/.ideavimrc
+ln -sf $HOME/dot-files/UltiSnips $HOME/.vim/UltiSnips
+
+nvim +PlugInstall +qall
+
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
+ln -sf ~/.vim $XDG_CONFIG_HOME/nvim
+ln -sf ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
 
-$HOME/.vim/plugged/YouCompleteMe/install.py --clang-completer 
+$HOME/.vim/plugged/YouCompleteMe/install.py #--clang-completer 
+
 sudo chsh -s $(which zsh)
-echo "Please Re login to apply the changes."
-echo "Press Enter."
-read NOP 
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
